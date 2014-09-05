3aa44ba109c5c0e0d552ab77407050137f38d72c
tianshiaimili <huyue52099>
2014-09-05 16:31:18 +0800

    asdasdasda

---
+++ src/com/baoyz/swipemenulistview/SwipeMenu.java
@@ -0,0 +1,42 @@
+package com.baoyz.swipemenulistview;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+
+/**
+ * 
+ * µÂÂêÎ÷ÑÇ
+ */
+public class SwipeMenu {
+
+	private Context mContext;
+	private List<SwipeMenuItem> mItems;
+
+	public SwipeMenu(Context context) {
+		mContext = context;
+		mItems = new ArrayList<SwipeMenuItem>();
+	}
+
+	public Context getContext() {
+		return mContext;
+	}
+
+	public void addMenuItem(SwipeMenuItem item) {
+		mItems.add(item);
+	}
+
+	public void removeMenuItem(SwipeMenuItem item) {
+		mItems.remove(item);
+	}
+
+	public List<SwipeMenuItem> getMenuItems() {
+		return mItems;
+	}
+	
+	public SwipeMenuItem getMenuItem(int index){
+		return mItems.get(index);
+	}
+}
---
+++ src/com/baoyz/swipemenulistview/SwipeMenuListView.java
@@ -0,0 +1,180 @@
+package com.baoyz.swipemenulistview;
+
+import android.R.interpolator;
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+/**
+ * 
+ * 
+ */
+public class SwipeMenuListView extends ListView {
+
+	private static final int TOUCH_STATE_NONE = 0;
+	private static final int TOUCH_STATE_X = 1;
+	private static final int TOUCH_STATE_Y = 2;
+
+	private int MAX_Y = 5;
+	private int MAX_X = 3;
+	private float mDownX;
+	private float mDownY;
+	private int mTouchState;
+	private int mTouchPosition;
+	private SwipeMenuLayout mTouchView;
+
+	private SwipeMenuCreator mMenuCreator;
+	private OnMenuItemClickListener mOnMenuItemClickListener;
+	private Interpolator mCloseInterpolator;
+	private Interpolator mOpenInterpolator;
+
+	public SwipeMenuListView(Context context) {
+		super(context);
+		init();
+	}
+
+	public SwipeMenuListView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init();
+	}
+
+	public SwipeMenuListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	private void init() {
+		MAX_X = dp2px(MAX_X);
+		MAX_Y = dp2px(MAX_Y);
+		mTouchState = TOUCH_STATE_NONE;
+	}
+
+	@Override
+	public void setAdapter(ListAdapter adapter) {
+		super.setAdapter(new SwipeMenuAdapter(getContext(), adapter) {
+			@Override
+			public void createMenu(SwipeMenu menu) {
+				if (mMenuCreator != null) {
+					mMenuCreator.create(menu);
+				}
+			}
+
+			@Override
+			public void onItemClick(SwipeMenuView view, SwipeMenu menu,
+					int index) {
+				if (mOnMenuItemClickListener != null) {
+					mOnMenuItemClickListener.onMenuItemClick(
+							view.getPosition(), menu, index);
+				}
+				if (mTouchView != null) {
+					mTouchView.smoothCloseMenu();
+				}
+			}
+		});
+	}
+	
+	public void setCloseInterpolator(Interpolator interpolator){
+		mCloseInterpolator = interpolator;
+	}
+	
+	public void setOpenInterpolator(Interpolator interpolator){
+		mOpenInterpolator = interpolator;
+	}
+	
+	public Interpolator getOpenInterpolator(){
+		return mOpenInterpolator;
+	}
+	
+	public Interpolator getCloseInterpolator(){
+		return mCloseInterpolator;
+	}
+	
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		return super.onInterceptTouchEvent(ev);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent ev) {
+		int action = MotionEventCompat.getActionMasked(ev); // TODO 寰呯爺绌�		action = ev.getAction();
+		switch (action) {
+		case MotionEvent.ACTION_DOWN:
+			mDownX = ev.getX();
+			mDownY = ev.getY();
+			mTouchState = TOUCH_STATE_NONE;
+
+			if (mTouchView != null && mTouchView.isOpen()) {
+				mTouchView.smoothCloseMenu();
+				return false;
+			}
+			mTouchPosition = pointToPosition((int) ev.getX(), (int) ev.getY());
+			View view = getChildAt(mTouchPosition - getFirstVisiblePosition());
+			if (view instanceof SwipeMenuLayout) {
+				mTouchView = (SwipeMenuLayout) view;
+			}
+			if (mTouchView != null) {
+				mTouchView.onSwipe(ev);
+			}
+			break;
+		case MotionEvent.ACTION_MOVE:
+			float dy = Math.abs((ev.getY() - mDownY));
+			float dx = Math.abs((ev.getX() - mDownX));
+			if (mTouchState == TOUCH_STATE_X) {
+				// 婊戝姩
+				if (mTouchView != null) {
+					mTouchView.onSwipe(ev);
+				}
+				getSelector().setState(new int[] { 0 });
+				ev.setAction(MotionEvent.ACTION_CANCEL);
+				super.onTouchEvent(ev);
+				return true;
+			} else {
+				// Log.i("byz", "state = " + mTouchState + ", dx = " + dx
+				// + ", max = " + MAX_X);
+				if (mTouchState != TOUCH_STATE_X && Math.abs(dy) > MAX_Y) {
+					mTouchState = TOUCH_STATE_Y;
+				}
+				if (mTouchState != TOUCH_STATE_Y && dx > MAX_X) {
+					mTouchState = TOUCH_STATE_X;
+				}
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			if (mTouchState == TOUCH_STATE_X) {
+				if (mTouchView != null) {
+					mTouchView.onSwipe(ev);
+				}
+				ev.setAction(MotionEvent.ACTION_CANCEL);
+				super.onTouchEvent(ev);
+				return true;
+			}
+			break;
+		}
+		return super.onTouchEvent(ev);
+	}
+
+	private int dp2px(int dp) {
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+				getContext().getResources().getDisplayMetrics());
+	}
+
+	public void setMenuCreator(SwipeMenuCreator menuCreator) {
+		this.mMenuCreator = menuCreator;
+	}
+
+	public void setOnMenuItemClickListener(
+			OnMenuItemClickListener onMenuItemClickListener) {
+		this.mOnMenuItemClickListener = onMenuItemClickListener;
+	}
+
+	public static interface OnMenuItemClickListener {
+		void onMenuItemClick(int position, SwipeMenu menu, int index);
+	}
+}
